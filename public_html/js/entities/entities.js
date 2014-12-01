610c01e4fa495530420bbfc3bf8e6b89f5005b94
JesusOsorioMorales <travon99707@yahoo.com>
2014-12-01 15:00:57 -0800

    video 16/17

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -1,6 +1,6 @@
- // TODO
- game.PlayerEntity = me.Entity.extend ({
-    init: function(x,y, settings) {
+// TODO
+game.PlayerEntity = me.Entity.extend({
+    init: function(x, y, settings) {
         this._super(me.Entity, 'init', [x, y, {
                 image: "mario",
                 spritewidth: "128",
@@ -8,59 +8,69 @@
                 width: 128,
                 height: 128,
                 getShape: function() {
-                    return  (new me.Rect(0, 0, 128, 128)).toPolygon();
+                    return  (new me.Rect(0, 0, 30, 128)).toPolygon();
                 }
-        }]);
-    
-    this.renderable.addAnimation("idle", [3]);
-    this.renderable.addAnimation("smallWalk", [8, 9, 10, 11, 12, 13], 80); 
-    
-    this.renderable.setCurrentAnimation("idle");
-            
-            this.body.setVelocity(5, 20);
-            
+            }]);
+
+        this.renderable.addAnimation("idle", [3]);
+        this.renderable.addAnimation("smallWalk", [8, 9, 10, 11, 12, 13], 80);
+
+        this.renderable.setCurrentAnimation("idle");
+
+        this.body.setVelocity(5, 20);
+        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+
     },
     
-     update:function(delta){
-        if (me.input.isKeyPressed("right")) {
-            this.body.vel.x += this.body.accel.x  * me.timer.tick; 
-            //this.renderable.setCurrentAnimation("smallWalk");
-        }else{
-            this.body.vel.x = 0; 
-        }
+    update: function(delta) {
         
+        if (me.input.isKeyPressed("right")) {
+            this.body.vel.x += this.body.accel.x * me.timer.tick;
+            //this.renderable.setCurrentAnimation("smallWalk");
+        } else {
+            this.body.vel.x = 0;
+        }
+
         this.body.update(delta);
         me.collision.check(this, true, this.collideHandler.bind(this), true);
-        
+
         if (this.body.vel.x !== 0) {
             if (!this.renderable.isCurrentAnimation("smallWalk")) {
-                 this.renderable.setCurrentAnimation("smallWalk");
-                 this.renderable.setAnimationFrame();
-            }    
-        }else{
+                this.renderable.setCurrentAnimation("smallWalk");
+                this.renderable.setAnimationFrame();
+            }
+        } else {
             this.renderable.setCurrentAnimation("idle");
         }
         
-        this.body.update(delta);
+  
+
+        
+
         this._super(me.Entity, "update", [delta]);
         return true;
-    }
+    },
     
-   // collideHandler: function(responce){
+    collideHandler: function(responce) {
         
-   // }
+    },
     
- });
- 
- game.LevelTrigger =  me.Entity.extend({
-     init: function (x, y,  settings) {
-         this._super(me,Entity, 'init', [x, y, setting]);
-         this.body.onCollision = this.onCollision.bind(this);
-     },
      
-     onCollision: function(){
-         this.body.setCollisionMask(me.collision.types.NO_OBJECT);
-         me.levelDirector.loadLevel(this.level);
-     }
-     
- });
+});
+
+game.LevelTrigger = me.Entity.extend({
+    init: function(x, y, settings) {
+        this._super(me.Entity, 'init', [x, y, settings]);
+        this.body.onCollision = this.onCollision.bind(this);
+        this.level = ("JesusLevel02");
+        this.xSpawn = settings.xSpawn;
+        this.ySpawn = settings.ySpawn;
+        },
+    onCollision: function() {
+        this.body.setCollisionMask(me.collision.types.NO_OBJECT);
+        me.levelDirector.loadLevel(this.level);
+        me.state.current().resetPlayer(this.xSpawn,this.ySpawn);
+        
+    }
+
+});
